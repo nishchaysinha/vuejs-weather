39561579113a2aa4220d16fad0d310315bac8416
Nishchay Sinha <nishchay.space@gmail.com>
2022-06-22 03:25:16 +0530

    added bgr image

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,43 @@
 <template>
   <div id="app">
-    Hello
+    <main>
+      <div class="search-box">
+        <input type="text" class="search-bar" placeholder="Search for a Place..."/>
+      </div>
+    </main>
   </div>
 </template>
 
 <script>
-
+  export default {
+    name: "app",
+    data(){
+      api_key: '323859ab1dd9648172b8ab428b2d89f5'
+    }
+  } 
 </script>
 
 <style>
+*{
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+body{
+  font-family: 'monserrat', sans-serif;
+
+}
+#app{
+  background-image: url('./assets/cold.jpg');
+  background-size: cover;
+  background-position: bottom;
+  transition: 0.4s;
+}
+
+main{
+  min-height: 100vh;
+  padding: 75px;
+}
 
 </style>
